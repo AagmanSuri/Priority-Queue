92a857cfcf9964e480fd18797c704a331a38453c
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2020-12-02 18:44:52 +0530

    insert

--- Priority Queue/main.c
+++ Priority Queue/main.c
@@ -8,3 +8,28 @@ struct node
     struct node *link;
 }*front = NULL;
 
+int flag=0,i;
+
+void insert(int item, int item_priority)
+{
+    struct node *tmp,*p;
+    tmp = (struct node *)malloc(sizeof(struct node));
+    tmp->info = item;
+    tmp->priority = item_priority;
+
+    if(front==NULL || item_priority < front->priority)
+    {
+        tmp->link = front;
+        front = tmp;
+    }
+    else
+    {
+        p = front;
+        while( p->link!=NULL && p->link->priority <= item_priority)
+        {
+            p = p->link;
+        }
+        tmp->link = p->link;
+        p->link = tmp;
+    }
+}
